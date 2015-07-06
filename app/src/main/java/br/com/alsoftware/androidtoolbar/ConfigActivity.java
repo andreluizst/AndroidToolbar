e2c773578d23ac204c7947cd20a7a2fb63f0c149
andreluizst <andre.luiz.st@hotmail.com>
2015-07-06 01:42:13 -0300

    atualização do projeto: Exemplo de toolbar finalizado.

--- app/src/main/java/br/com/alsoftware/androidtoolbar/ConfigActivity.java
+++ app/src/main/java/br/com/alsoftware/androidtoolbar/ConfigActivity.java
@@ -1,31 +1,30 @@
 package br.com.alsoftware.androidtoolbar;
 
-//import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by AndreLuiz on 05/07/2015.
+ */
+public class ConfigActivity extends AppCompatActivity {
     private Toolbar mToolbarPrincipal;
     private Toolbar mToolbarRodape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_config);
 
         mToolbarPrincipal = (Toolbar)findViewById(R.id.tb_principal);
-        mToolbarPrincipal.setTitle("AndroidToolbar");
-        mToolbarPrincipal.setSubtitle("Exemplo");
-        mToolbarPrincipal.setLogo(R.mipmap.ic_launcher);
+        mToolbarPrincipal.setTitle("ConfigActivity");
         setSupportActionBar(mToolbarPrincipal);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mToolbarRodape = (Toolbar)findViewById(R.id.tb_Included_Rodape);
         mToolbarRodape.inflateMenu(R.menu.menu_toolbar_rodape);
@@ -51,38 +50,16 @@ public class MainActivity extends AppCompatActivity {
         mToolbarRodape.findViewById(R.id.img_config).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Menu de configuraçoes acionado!", Toast.LENGTH_SHORT).show();
-                startConfigActivity();
+                Toast.makeText(ConfigActivity.this, "Menu de configuraçoes acionado!", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.mn_Config) {
-            startConfigActivity();
+        if (item.getItemId() == android.R.id.home){
+            finish();
         }
-
         return true;
     }
-
-    private void startConfigActivity(){
-        Intent it = new Intent(this, ConfigActivity.class);
-        startActivity(it);
-    }
-
-
 }
